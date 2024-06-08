6340e543bb53617d536348ee744c1ea6bdabae70
Carlton Ncube <carltonncube@outlook.com>
2024-06-08 23:33:15 +0200

    alx final software engineering project

---
+++ backend/vibe_music_app/build_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Collect static files
+echo "Collecting static files"
+python manage.py collectstatic --noinput
+
+# Create directory for build files if it doesn't exist
+mkdir -p staticfiles_build
+
+# Copy static files to the build directory
+cp -r staticfiles/ staticfiles_build/
+
+# installing requirements
+pip install -r requirements.txt
--- backend/vibe_music_app/vibe_music_app/urls.py
+++ backend/vibe_music_app/vibe_music_app/urls.py
@@ -1,4 +1,5 @@
-"""vibe_music_app URL Configuration
+"""
+vibe_music_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('music.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
